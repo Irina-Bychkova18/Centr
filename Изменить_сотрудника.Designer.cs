ff3131b70717e1363e98624e6477c904c89f05e0
Irina-Bychkova18 <bichkovairina505@gmail.com>
2024-04-11 19:48:53 +0300

    Добавлены и сделаны формы: Видео-лекции, все видеоролики админ, добавить видеоролик Добавлены ссылки на лекции и видео-лекции Добавлена форма Изменить_сотрудника

---
+++ Изменить_сотрудника.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace Centr
+{
+    partial class Изменить_сотрудника
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Изменить_сотрудника));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Отменить_добавление_button1 = new System.Windows.Forms.Button();
+            this.Ведет_курс_comboBox4 = new System.Windows.Forms.ComboBox();
+            this.Должность_comboBox3 = new System.Windows.Forms.ComboBox();
+            this.Опыт_comboBox2 = new System.Windows.Forms.ComboBox();
+            this.Возраст_comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Пароль_textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Логин_textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Телефон_textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Выход_button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ФИО_textBox1 = new System.Windows.Forms.TextBox();
+            this.Изменить_button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(801, 453);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.AliceBlue;
+            this.tabPage1.Controls.Add(this.Отменить_добавление_button1);
+            this.tabPage1.Controls.Add(this.Ведет_курс_comboBox4);
+            this.tabPage1.Controls.Add(this.Должность_comboBox3);
+            this.tabPage1.Controls.Add(this.Опыт_comboBox2);
+            this.tabPage1.Controls.Add(this.Возраст_comboBox1);
+            this.tabPage1.Controls.Add(this.Пароль_textBox7);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.Логин_textBox8);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.Телефон_textBox5);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.Выход_button);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.ФИО_textBox1);
+            this.tabPage1.Controls.Add(this.Изменить_button2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(793, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Добавить сотрудника";
+            // 
+            // Отменить_добавление_button1
+            // 
+            this.Отменить_добавление_button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Отменить_добавление_button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Отменить_добавление_button1.Location = new System.Drawing.Point(178, 331);
+            this.Отменить_добавление_button1.Name = "Отменить_добавление_button1";
+            this.Отменить_добавление_button1.Size = new System.Drawing.Size(203, 30);
+            this.Отменить_добавление_button1.TabIndex = 108;
+            this.Отменить_добавление_button1.Text = "Отменить добавление";
+            this.Отменить_добавление_button1.UseVisualStyleBackColor = false;
+            // 
+            // Ведет_курс_comboBox4
+            // 
+            this.Ведет_курс_comboBox4.FormattingEnabled = true;
+            this.Ведет_курс_comboBox4.Location = new System.Drawing.Point(178, 228);
+            this.Ведет_курс_comboBox4.Name = "Ведет_курс_comboBox4";
+            this.Ведет_курс_comboBox4.Size = new System.Drawing.Size(453, 24);
+            this.Ведет_курс_comboBox4.TabIndex = 107;
+            // 
+            // Должность_comboBox3
+            // 
+            this.Должность_comboBox3.FormattingEnabled = true;
+            this.Должность_comboBox3.Location = new System.Drawing.Point(178, 185);
+            this.Должность_comboBox3.Name = "Должность_comboBox3";
+            this.Должность_comboBox3.Size = new System.Drawing.Size(453, 24);
+            this.Должность_comboBox3.TabIndex = 106;
+            // 
+            // Опыт_comboBox2
+            // 
+            this.Опыт_comboBox2.FormattingEnabled = true;
+            this.Опыт_comboBox2.Location = new System.Drawing.Point(434, 102);
+            this.Опыт_comboBox2.Name = "Опыт_comboBox2";
+            this.Опыт_comboBox2.Size = new System.Drawing.Size(197, 24);
+            this.Опыт_comboBox2.TabIndex = 105;
+            // 
+            // Возраст_comboBox1
+            // 
+            this.Возраст_comboBox1.FormattingEnabled = true;
+            this.Возраст_comboBox1.Location = new System.Drawing.Point(178, 102);
+            this.Возраст_comboBox1.Name = "Возраст_comboBox1";
+            this.Возраст_comboBox1.Size = new System.Drawing.Size(197, 24);
+            this.Возраст_comboBox1.TabIndex = 104;
+            // 
+            // Пароль_textBox7
+            // 
+            this.Пароль_textBox7.Location = new System.Drawing.Point(436, 274);
+            this.Пароль_textBox7.Name = "Пароль_textBox7";
+            this.Пароль_textBox7.Size = new System.Drawing.Size(195, 22);
+            this.Пароль_textBox7.TabIndex = 103;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(369, 277);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 17);
+            this.label7.TabIndex = 102;
+            this.label7.Text = "Пароль:";
+            // 
+            // Логин_textBox8
+            // 
+            this.Логин_textBox8.Location = new System.Drawing.Point(178, 274);
+            this.Логин_textBox8.Name = "Логин_textBox8";
+            this.Логин_textBox8.Size = new System.Drawing.Size(185, 22);
+            this.Логин_textBox8.TabIndex = 101;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(73, 277);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 17);
+            this.label8.TabIndex = 100;
+            this.label8.Text = "Логин:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(381, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 17);
+            this.label6.TabIndex = 98;
+            this.label6.Text = "Опыт:";
+            // 
+            // Телефон_textBox5
+            // 
+            this.Телефон_textBox5.Location = new System.Drawing.Point(178, 142);
+            this.Телефон_textBox5.Name = "Телефон_textBox5";
+            this.Телефон_textBox5.Size = new System.Drawing.Size(453, 22);
+            this.Телефон_textBox5.TabIndex = 97;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 17);
+            this.label5.TabIndex = 96;
+            this.label5.Text = "Телефон:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 94;
+            this.label4.Text = "Возраст:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(678, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 93;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Выход_button
+            // 
+            this.Выход_button.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Выход_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Выход_button.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.Выход_button.Location = new System.Drawing.Point(11, 6);
+            this.Выход_button.Name = "Выход_button";
+            this.Выход_button.Size = new System.Drawing.Size(121, 38);
+            this.Выход_button.TabIndex = 53;
+            this.Выход_button.Text = "Выход";
+            this.Выход_button.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Ведет курс:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Должность:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "ФИО:";
+            // 
+            // ФИО_textBox1
+            // 
+            this.ФИО_textBox1.Location = new System.Drawing.Point(178, 62);
+            this.ФИО_textBox1.Name = "ФИО_textBox1";
+            this.ФИО_textBox1.Size = new System.Drawing.Size(453, 22);
+            this.ФИО_textBox1.TabIndex = 38;
+            // 
+            // Изменить_button2
+            // 
+            this.Изменить_button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Изменить_button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Изменить_button2.Location = new System.Drawing.Point(412, 331);
+            this.Изменить_button2.Name = "Изменить_button2";
+            this.Изменить_button2.Size = new System.Drawing.Size(219, 30);
+            this.Изменить_button2.TabIndex = 37;
+            this.Изменить_button2.Text = "Изменить";
+            this.Изменить_button2.UseVisualStyleBackColor = false;
+            // 
+            // Изменить_сотрудника
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Изменить_сотрудника";
+            this.Text = "Изменить_сотрудника";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button Отменить_добавление_button1;
+        private System.Windows.Forms.ComboBox Ведет_курс_comboBox4;
+        private System.Windows.Forms.ComboBox Должность_comboBox3;
+        private System.Windows.Forms.ComboBox Опыт_comboBox2;
+        private System.Windows.Forms.ComboBox Возраст_comboBox1;
+        private System.Windows.Forms.TextBox Пароль_textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Логин_textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Телефон_textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Выход_button;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ФИО_textBox1;
+        private System.Windows.Forms.Button Изменить_button2;
+    }
+}
